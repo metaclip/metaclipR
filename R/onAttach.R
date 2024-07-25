9afa4f06945360bba0c43b0368b7f885c39594cb
jbedia <joaquin.bedia@gmail.com>
2024-07-25 18:32:46 +0200

    Suppress remote ontology checks on attach (slows down package load)

--- R/onAttach.R
+++ R/onAttach.R
@@ -7,17 +7,17 @@
     builddate <- packageDescription(pkgname)$Date
     mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")
     packageStartupMessage(mess)
-    refURL <- paste0("http://metaclip-interpreter.predictia.es/individuals?vocab=http://www.metaclip.org/datasource&class=GCM") %>% URLencode() %>% url()
-    on.exit(close(refURL))
-    out <- tryCatch(suppressWarnings(readLines(refURL, warn = FALSE)), error = function(er) {
-        er <- NULL
-        return(er)
-    })
-    if (!is.null(out)) {
-        packageStartupMessage("Succesfully opened the connection with remote ontology files")
-    } else {
-        packageStartupMessage("WARNING: Unable to open a connection with remote ontology files\nFull metadata details may be incomplete")
-    }
+    # refURL <- paste0("http://metaclip-interpreter.predictia.es/individuals?vocab=http://www.metaclip.org/datasource&class=GCM") %>% URLencode() %>% url()
+    # on.exit(close(refURL))
+    # out <- tryCatch(suppressWarnings(readLines(refURL, warn = FALSE)), error = function(er) {
+    #     er <- NULL
+    #     return(er)
+    # })
+    # if (!is.null(out)) {
+    #     packageStartupMessage("Succesfully opened the connection with remote ontology files")
+    # } else {
+    #     packageStartupMessage("WARNING: Unable to open a connection with remote ontology files\nFull metadata details may be incomplete")
+    # }
 }
 # End
 
